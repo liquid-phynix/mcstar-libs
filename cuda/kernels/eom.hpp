1f1794c4ad134c007fa16b8f23c6ac31a5831f87
Dexter Filmore <liquid.phynix@gmail.com>
2015-11-08 17:56:35 +0100

    eom kernel arguments fixed

--- cuda/kernels/eom.hpp
+++ cuda/kernels/eom.hpp
@@ -12,7 +12,8 @@ __global__ void kernel_update_eom(Float2* arr_kpsi, Float2* arr_knonlin, Float2*
                       (kpsi.y + dt * dop.x * knonlin.y + dt * knoise.y) / denom }; }
 */
 
-__global__ void kernel_update_eom(Float2* arr_kpsi, Float2* arr_knonlin, Float2* arr_knoise, Float namp, int3 rdims, int3 cdims, Float dt, Float eps, Float3 lens){
+__global__ void kernel_update_eom(Float2* arr_kpsi, Float2* arr_knonlin, Float2* arr_knoise,
+                                  int3 rdims, int3 cdims, Float namp, Float dt, Float eps, Float3 lens){
     IDX012(cdims);
     const Float2 kpsi = arr_kpsi[idx];
     const Float2 knonlin = arr_knonlin[idx];
@@ -23,7 +24,8 @@ __global__ void kernel_update_eom(Float2* arr_kpsi, Float2* arr_knonlin, Float2*
     arr_kpsi[idx] = { (kpsi.x + dt * dop.x * knonlin.x + dt * namp * knoise.x) / denom,
                       (kpsi.y + dt * dop.x * knonlin.y + dt * namp * knoise.y) / denom }; }
 
-void call_kernel_update_eom(GPUArray& arr_kpsi, GPUArray& arr_knonlin, GPUArray& arr_knoise, Float namp, Float dt, Float eps, Float3 lens){
+void call_kernel_update_eom(GPUArray& arr_kpsi, GPUArray& arr_knonlin, GPUArray& arr_knoise,
+                            Float namp, Float dt, Float eps, Float3 lens){
   Launch l(arr_kpsi.cmpl_vext());
   kernel_update_eom<<<l.get_gs(), l.get_bs()>>>(arr_kpsi.ptr_cmpl(), arr_knonlin.ptr_cmpl(), arr_knoise.ptr_cmpl(), arr_kpsi.real_vext(), arr_kpsi.cmpl_vext(), namp, dt, eps, lens);
   CUERR(cudaThreadSynchronize()); }
