9ee72ce4ed41a34752f851b6eeca2f8bd3f94103
Dexter Filmore <liquid.phynix@gmail.com>
2015-11-08 16:31:56 +0100

    eom kernel takes unit normal noise genrated directly in spectrum

--- cuda/kernels/eom.hpp
+++ cuda/kernels/eom.hpp
@@ -1,5 +1,17 @@
 #include <cuda/kernels/common.hpp>
 
+/*
+__global__ void kernel_update_eom(Float2* arr_kpsi, Float2* arr_knonlin, Float2* arr_knoise, int3 rdims, int3 cdims, Float dt, Float eps, Float3 lens){
+    IDX012(cdims);
+    const Float2 kpsi = arr_kpsi[idx];
+    const Float2 knonlin = arr_knonlin[idx];
+    const Float2 knoise = arr_knoise[idx];
+    const Float2 dop = L_L2(K(i0, rdims.x, lens.x), K(i1, rdims.y, lens.y), K(i2, rdims.z, lens.z));
+    const Float denom = Float(1) - dt * ((Float(2) - eps) * dop.x + Float(2) * dop.y + dop.x * dop.y);
+    arr_kpsi[idx] = { (kpsi.x + dt * dop.x * knonlin.x + dt * knoise.x) / denom,
+                      (kpsi.y + dt * dop.x * knonlin.y + dt * knoise.y) / denom }; }
+*/
+
 __global__ void kernel_update_eom(Float2* arr_kpsi, Float2* arr_knonlin, Float2* arr_knoise, int3 rdims, int3 cdims, Float dt, Float eps, Float3 lens){
     IDX012(cdims);
     const Float2 kpsi = arr_kpsi[idx];
